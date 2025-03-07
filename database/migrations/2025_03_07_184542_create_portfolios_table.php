fde09ee928b42214085891655aa7134b202d8afa
AlexRitter777 <bogachev@email.cz>
2025-03-07 20:09:18 +0100

    Version V0.2.1 - Added migrations for services and portfolio tables - Minor changes

---
+++ database/migrations/2025_03_07_184542_create_portfolios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('name');
+            $table->string('type');
+            $table->string('purpose');
+            $table->text('features');
+            $table->text('requirements');
+            $table->text('description');
+            $table->text('content');
+            $table->json('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portfolios');
+    }
+};
